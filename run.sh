191b8653c1bfae3a366769e6d4484c8249555f54
Bert Burgemeister <trebbu@googlemail.com>
2021-10-08 16:04:15 +0200

    Add The Original Legal Musicians' Fake Book

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@
 # vol_h1.txt ... vol_h6.txt: Real Book (Hal Leonard) vol 1-6
 # vol_jf.txt: The Jazz Fakebook (JazzFake)
 # vol_jl.txt: Jazz LTD (JazzLTD)
+# vol_ol: The Original Legal Musicians' Fake Book
 # vol_lm.txt: Library Of Musician’s Jazz (Library)
 # vol_n1.txt ... vol_n3.txt: The New Real Book (Sher) vol 1-3 (NewReal1-3)
 # vol_rj.txt: The Real Jazz Book (Hal Leonard)
@@ -28,13 +29,14 @@ grep -e '^NewReal3' MASTERNX.tmp | cut -d ' ' -f 2- | sed -e 's/\(.*\) (\(The\|A
 grep -e '^Library'  MASTERNX.tmp | cut -d ' ' -f 2- | sed -e 's/\(.*\) (\(The\|A\))/\2 \1/g' > vol_lm.txt
 sed -e 's/\(.*\), \(the\|a\)/\2 \1/g' thebook.txt > vol_bc.txt
 cp the_worlds_greatest_fakebook.txt vol_wg.txt
+cp original-legal-musician-fake-book.txt vol_ol.txt
 cut -d ' ' -f 2- real_jazz_book.txt | awk '{$1=$1};1' > vol_rj.txt
 sed -e 's/\(.*\), \(The\|A\)/\2 \1/g' vol_st_raw.txt > vol_st.txt
 for i in {1..6}; do
     cp vol_h${i}_raw.txt vol_h$i.txt
 done
 
-for v in {h1,u1,n1,h2,u2,n2,h3,u3,n3,h4,h5,h6,st,jl,jf,cc,lm,bc,ev,wg,rj}; do
+for v in {h1,u1,n1,h2,u2,n2,h3,u3,n3,h4,h5,h6,st,jl,jf,cc,lm,bc,ev,wg,rj,ol}; do
     awk -v vol=$v '{print toupper($0) "%%--%%" toupper(vol)}' vol_$v.txt
 done | sed -e 's/&/\\&/g' -e 's/#/$\\sharp$/' | \
     sort | \
