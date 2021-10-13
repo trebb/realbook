883a3799044dfd128a0ae81cc94fda6abbee9bbb
Bert Burgemeister <trebbu@googlemail.com>
2021-10-13 15:09:53 +0200

    Add new content

--- run.sh
+++ run.sh
@@ -2,23 +2,23 @@
 
 # vol_bc.txt: The Book - Commercial (thebook.txt)
 # vol_cc.txt: The Colorado Cookbook (MASTERNX.txt:ColoBk)
+# vol_eu.txt: The European Real Book (Sher Music) (the-european-real-book.txt)
 # vol_ev.txt: Bill Evans Fakebook (MASTERNX.txt:EvansBk)
+# vol_fj.txt: 557 Jazz Standards - swing to bop (557-jazz-standards.txt)
 # vol_h1.txt ... vol_h6.txt: Real Book (Hal Leonard) vol 1-6 (vol_h{1..6}_raw.txt)
 # vol_jf.txt: The Jazz Fakebook (MASTERNX.txt:JazzFake)
+# vol_jj.txt: Just Jazz Real Book (Hal Leonard) (just-jazz-real-book.txt)
 # vol_jl.txt: Jazz LTD (MASTERNX.txt:JazzLTD)
-# vol_ol: The Original Legal Musicians' Fake Book (original-legal-musician-fake-book.txt)
+# vol_js.txt: Just Standards Real Book (Warner Brothers) (just-standards-real-book.txt)
 # vol_lm.txt: Library Of Musician’s Jazz (MASTERNX.txt:Library)
 # vol_n1.txt ... vol_n3.txt: The New Real Book (Sher) vol 1-3 (MASTERNX.txt:NewReal1-3)
+# vol_ol: The Original Legal Musicians' Fake Book (original-legal-musician-fake-book.txt)
 # vol_rj.txt: The Real Jazz Book (Hal Leonard) (real_jazz_book.txt)
+# vol_rs.txt: The Real Jazz Standards Fake Book (Hal Leonard) (real-jazz-standards-fake-book.txt)
 # vol_st.txt: Standards Real Book (Sher Music) (vol_st_raw.txt)
 # vol_u1.txt ... vol_v3.txt: The Real Book (5th edition) vol 1-3 (MASTERNX.txt:RealBk1-3)
+# vol_uj.txt: The Ultimate Jazz Fake Book (Hal Leonard) (ultimate-jazz-fake-book.txt)
 # vol_wg.txt: The World's Greatest Fake Book (Sher Music) (the_worlds_greatest_fakeboot.txt)
-# Just Standards Real Book (Warner Brothers) ISBN 9780757904530 (just-standards-real-book.txt)
-# Just Jazz Real Book (Hal Leonard) ISBN 9780757901683 (just-jazz-real-book.txt)
-# The Real Jazz Standards Fake Book (Hal Leonard) ISBN 9780634021558 (real-jazz-standards-fake-book.txt)
-# The Ultimate Jazz Fake Book (Hal Leonard) ISBN 9780881889796 (ultimate-jazz-fake-book.txt)
-# 557 Jazz Standards - swing to bop (557-jazz-standards.txt)
-# The European Real Book (Sher Music) ISBN‎ 9781883217242 (the-european-real-book.txt)
 
 
 awk -e '{print $(NF-1), $0}' MASTERNX.txt | awk -e 'NF{NF-=2};1' > MASTERNX.tmp
@@ -36,20 +36,28 @@ grep -e '^Library'  MASTERNX.tmp | cut -d ' ' -f 2- | sed -e 's/\(.*\) (\(The\|A
 sed -e 's/\(.*\), \(the\|a\)/\2 \1/g' thebook.txt > vol_bc.txt
 cp the_worlds_greatest_fakebook.txt vol_wg.txt
 cp original-legal-musician-fake-book.txt vol_ol.txt
+
+cp just-standards-real-book.txt vol_js.txt
+sed -e 's/\(.*\), \(THE\|A\)/\2 \1/g' just-jazz-real-book.txt > vol_jj.txt
+cp real-jazz-standards-fake-book.txt vol_rs.txt
+sed -e 's/\(.*\), \(The\|A\)/\2 \1/g' ultimate-jazz-fake-book.txt > vol_uj.txt
+sed -e 's/\(.*\), \(The\|A\)/\2 \1/g' 557-jazz-standards.txt > vol_fj.txt
+cp the-european-real-book.txt vol_eu.txt
+
 cut -d ' ' -f 2- real_jazz_book.txt | awk '{$1=$1};1' > vol_rj.txt
 sed -e 's/\(.*\), \(The\|A\)/\2 \1/g' vol_st_raw.txt > vol_st.txt
 for i in {1..6}; do
     cp vol_h${i}_raw.txt vol_h$i.txt
 done
 
-for v in {h1,u1,n1,h2,u2,n2,h3,u3,n3,h4,h5,h6,st,jl,jf,cc,lm,bc,ev,wg,rj,ol}; do
+for v in {h1,u1,n1,h2,u2,n2,h3,u3,n3,h4,h5,h6,st,jl,jf,cc,lm,bc,ev,wg,rj,ol,js,jj,rs,uj,fj,eu}; do
     awk -v vol=$v '{print toupper($0) "%%--%%" toupper(vol)}' vol_$v.txt
 done > tempfile0
 sed -e 's/&/\\&/g' -e 's/#/$\\sharp$/' tempfile0 | \
     sort | \
-    awk -F '%%--%%'  'vol[$1]=vol[$1] "," $2 {print $1, "%%--%%" vol[$1]}' | \
+    awk -F '%%--%%'  'vol[$1]=vol[$1] ",\\allowbreak " $2 {print $1, "%%--%%" vol[$1]}' | \
     sort -r | \
-    awk -F '%%--%%,' '!seen[$1] {print "\\hangindent1em " $1 "\\nolinebreak\\dotfill\\nolinebreak\\rule{.5mm}{0mm}" $2 "\\par"; seen[$1]=1} ' > tempfile
+    awk -F '%%--%%,' '!seen[$1] {print "\\hangindent1em " $1 "\\nolinebreak\\dotfill\\nolinebreak\\rule{1mm}{0mm}" $2 "\\par"; seen[$1]=1} ' > tempfile
 
 for i in {A..Z}; do
     echo "%\\hangindent1em ${i}%%00000" >> tempfile
