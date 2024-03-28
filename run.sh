36f88896e13aa7a53296666c8691f500e79b285e
Bert Burgemeister <trebbu@googlemail.com>
2024-03-28 09:11:06 +0100

    Fix recent addition

--- run.sh
+++ run.sh
@@ -214,4 +214,4 @@ echo "checking for duplications..."
 grep -v "^$" prunedstrings | uniq -w 40 -D
 sed -e 's/\(^.*\)%%--%%.*%%--%%.*}\(.*\)}/\1==\2/' tempfile0 | sort | uniq -D
 
-echo "(expected duplications: LETSFALLINLOVE LOVESTORY MYLOVE NOCTURNE OURLOVEAFFAIR PARISBLUES PROMISE SOLAMENTEUNAVEZ THEME VERACRUZ)"
+echo "(expected duplications: LETSFALLINLOVE LOVESTORY MYLOVE NOCTURNE OURLOVEAFFAIR PARISBLUES PROMISE SOLAMENTEUNAVEZ THEME VERACRUZ WEE)"
